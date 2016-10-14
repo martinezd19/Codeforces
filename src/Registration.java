ffdd6a157be0fcf6e80e3d8de93cd5cf5f9ab00b
Dominic Martinez <martinezd19@trinityprep.org>
2016-10-13 22:26:55 -0400

    Sync Registration

--- src/Registration.java
+++ src/Registration.java
@@ -1,3 +1,15 @@
+import java.util.*;
+
 public class Registration {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int lines = Integer.parseInt(in.nextLine());
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < lines; i++) {
+            String name = in.nextLine();
+            if(!map.containsKey(name)) {
+                
+            }
+        }
+    }
 }
